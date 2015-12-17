98c6f7b00cc3a6567a311f7080acb1153518016d
gyane <gyanexus7@gmail.com>
2015-12-17 19:19:12 +0900

    routesにdrink/review追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
   }
   root to: 'indexes#index'
 
+  resources :drinks do
+    resources :reviews, :controller => 'drinks/reviews'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
