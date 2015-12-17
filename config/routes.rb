b9db89f1a7c7af5be51c898377ab1b23952d2786
gyane <gyanexus7@gmail.com>
2015-12-17 22:59:39 +0900

    レビューのroutes追加

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Rails.application.routes.draw do
     resources :reviews, :controller => 'drinks/reviews'
   end
 
+  namespace :stores do
+    resources :reviews, :controller => 'reviews'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
