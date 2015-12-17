8f32da08398dcb6a824f7652a9c5a31d41c4c32c
gyane <gyanexus7@gmail.com>
2015-12-17 23:11:39 +0900

    stores/favorites の routesを追加

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
 
   namespace :stores do
     resources :reviews, :controller => 'reviews'
+    resources :favorites, :controller => 'favorites'
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
