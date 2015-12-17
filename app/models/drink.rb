ebf8050b3a0f60788caf7ecb31d0eae68a5b6b1f
gyane <gyanexus7@gmail.com>
2015-12-17 19:17:17 +0900

    drinkモデルを作成

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,11 +1,12 @@
 class Drink < ActiveRecord::Base
 
-  has_many :drink_reviews, dependent: :destroy
+  has_many :reviews, class_name: "DrinkReview", foreign_key: :drink_id, dependent: :destroy
+  has_many :users_who_reviews, class_name: "User", through: :reviews
 
   belongs_to :brewers, foreign_key: :brewer_id
   belongs_to :alcoholics, foreign_key: :alcoholic_id
-  
-  has_and_belongs_to_many :users
+
+  has_and_belongs_to_many :users_who_likes, class_name: "User"
   has_and_belongs_to_many :stores
 
 end
