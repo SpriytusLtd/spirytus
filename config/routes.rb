06388e39360e99b5e64091e22f160237842d4123
gyane <gyanexus7@gmail.com>
2015-12-17 21:50:07 +0900

    drink/favorites のroutesを作成

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   resources :drinks, :controller => 'drinks/indexes' do
     resources :reviews, :controller => 'drinks/reviews'
+    resources :favorites, :controller => 'drinks/favorites'
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
