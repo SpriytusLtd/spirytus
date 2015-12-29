30ed3b01c577d23b26492cb54a19598576c256ce
ayato1995 <ayato.k070206@outlook.jp>
2015-12-29 02:20:13 +0000

    destroyの不具合が存在します

--- app/controllers/drinks/indexes_controller.rb
+++ app/controllers/drinks/indexes_controller.rb
@@ -1,2 +1,45 @@
 class Drinks::IndexesController < ApplicationController
+  def index
+    @drinks = Drink.all
+  end
+
+  def create
+    @drinks = Drink.new(drinks_params)
+    if @drinks.save
+      redirect_to drinks_path
+    else
+      render 'create'
+    end
+  end
+
+  def new
+    @drinks = Drink.new
+  end
+
+  def edit
+    @drinks = Drink.find(params[:id])
+  end
+ 
+  def show
+    @drinks = Drink.find(params[:id])
+  end
+
+  def update
+    @drinks = Drink.find(params[:id])
+    if @drinks.update(drinks_params)
+      redirect_to drinks_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @drinks = Drink.find(params[:id])
+    @drinks.destroy
+  end
+
+  private
+    def drinks_params
+      params[:drink].permit(:name, :alcohol, :detail)
+    end
 end
--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,6 +1,8 @@
 class Drink < ActiveRecord::Base
   has_many :reviews, class_name:  'DrinkReview', foreign_key: :drink_id, dependent: :destroy
   has_many :users_who_reviews, class_name: 'User', through: :reviews
+  has_many :store_drinks
+  has_many :stores, through: :store_drinks
 
   belongs_to :brewer, foreign_key: :brewer_id
   belongs_to :alcoholic, foreign_key: :alcoholic_id
