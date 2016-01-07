9478a6ce72c93de343cca96ae4a4dfa795b04813
ayato1995 <ayato.k070206@outlook.jp>
2016-01-07 08:53:24 +0000

    has_and_belong_manyのstores多重化をコメントアウトで削除してます

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -7,6 +7,6 @@ class Drink < ActiveRecord::Base
   belongs_to :brewer, foreign_key: :brewer_id
   belongs_to :alcoholic, foreign_key: :alcoholic_id
 
-  has_and_belongs_to_many :users_who_likes, class_name: 'User'
-  has_and_belongs_to_many :stores
+  #has_and_belongs_to_many :users_who_likes, class_name: 'User'
+  #has_and_belongs_to_many :stores
 end
