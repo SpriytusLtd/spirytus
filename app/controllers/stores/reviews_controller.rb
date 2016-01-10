32752bedd608592bd006b1b5845b6dbdb9400a16
tokikaze0604 <tokikaze0604@gmail.com>
2016-01-10 07:01:49 +0000

    作業途中

--- app/controllers/stores/reviews_controller.rb
+++ app/controllers/stores/reviews_controller.rb
@@ -1,53 +1,49 @@
 class Stores::ReviewsController < ApplicationController
+before_action :authenticate_user!
+protect_from_forgery except: [:index, :review_params, :new, :create, :show, :edit, :update, :destroy]
+
   def index
-    store = Store.find(params[:store_id])
-    @reviews = store
+    @store = Store.find_by(params[:store_id])
     end
 
   def new
-   @reviews = StoreReview.new
+   @review = StoreReview.new
    end
 
    def create
-    @reviews = StoreReview.new(store_reviews_params)
-    @reviews.user_id = current_user.id
+    @reviews = StoreReview.new(reviews_params)
     @reviews.store_id = params[:store_id]
-
+    @reviews.user_id = current_user.id
    if @reviews.save
      redirect_to :action => 'index', notice: 'Store review was successfully created.'
    else
      redirect_to :action => 'index', notice: 'failed'
    end
   end
-
   def show
-    @reviews = StoreReview.find(params[:id])
+    @reviews = StoreReview.find_by(params[:id])
   end
 
-  private
-
-    def store_id
-     params.permit(:store_id)
-    end
-
-    def store_reviews_params
-     params.require(:store_reviews).permit(:body)
-   end
 
   def edit
-   @reviews = StoreReview.find(params[:id])
+   @review = StoreReview.find_by(params[:id])
   end
 
   def update
-    @reviews = StoreReview.find(param[:id])
+    @reviews = StoreReview.find_by(params[:id])
     @reviews.update(review_params)
     redirect_to store_reviews_path
    end
 
    def destroy
-    @reviews = StoreReview.find(param[:id])
+    @reviews = StoreReview.find_by(params[:id])
     @reviews.destroy
 
     redirect_to store_reviews_path
-   end
   end
+
+#private
+  def review_params
+   params.require(:store_review).permit(:id,:store_id,:user_id,:body,:param1,:param2,:param3,:param4,:param5,:param6,:created_at,:updated_at)
+ end
+end
