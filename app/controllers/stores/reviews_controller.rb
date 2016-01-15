17b62afddd62ea5aea0b2a7b2e9674fa7749498a
gyane <gyanexus7@gmail.com>
2016-01-15 22:40:50 +0900

    rubocopさんの怒りを鎮めた

--- app/controllers/stores/reviews_controller.rb
+++ app/controllers/stores/reviews_controller.rb
@@ -1,55 +1,53 @@
 class Stores::ReviewsController < ApplicationController
-before_action :authenticate_user!
-protect_from_forgery except: [:index, :review_params, :new, :create, :show, :edit, :update, :destroy, :body]
+  before_action :authenticate_user!
+  protect_from_forgery except: [:index, :review_params, :new, :create, :show, :edit, :update, :destroy, :body]
 
   def index
     @store = Store.find_by(params[:store_id])
-    end
+  end
 
   def new
-   @review = StoreReview.new
-   end
+    @review = StoreReview.new
+  end
 
-   def create
+  def create
     @reviews = StoreReview.new(review_params)
     @reviews.store_id = params[:store_id]
-    @reviews.user_id = current_user.id 
-   if @reviews.save
-     redirect_to :action => 'index', notice: 'Store review was successfully created.'
-   else
-     redirect_to :action => 'index', notice: 'failed'
-   end
+    @reviews.user_id = current_user.id
+    if @reviews.save
+      redirect_to action: 'index', notice: 'Store review was successfully created.'
+    else
+      redirect_to action: 'index', notice: 'failed'
+    end
   end
 
   def show
     @reviews = StoreReview.find_by(params[:id])
   end
 
-
   def edit
-   @review = StoreReview.find_by(params[:id])
+    @review = StoreReview.find_by(params[:id])
   end
 
   def update
     @reviews = StoreReview.find_by(params[:id])
     @reviews.update(review_params)
     redirect_to store_reviews_path
-   end
+  end
 
-   def destroy
+  def destroy
     @reviews = StoreReview.find_by(params[:id])
     @reviews.destroy
 
     redirect_to store_reviews_path
   end
 
-#private
+  private
 
-def body
-
-end
+  def body
+  end
 
   def review_params
-   params.require(:store_review).permit(:id,:store_id,:user_id,:body,:created_at,:updated_at)
- end
+    params.require(:store_review).permit(:id, :store_id, :user_id, :body, :created_at, :updated_at)
+  end
 end
