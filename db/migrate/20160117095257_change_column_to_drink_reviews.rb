bbdbc9748306e8d5e4aa32f1747440147d4cc050
tokikaze0604 <tokikaze0604@gmail,com>
2016-01-17 20:11:22 +0900

    DrinkReviewのカラムの制約を変更

---
+++ db/migrate/20160117095257_change_column_to_drink_reviews.rb
@@ -0,0 +1,16 @@
+class ChangeColumnToDrinkReviews < ActiveRecord::Migration
+  def up
+    change_column :drink_reviews, :user_id, :integer, null: false, default: 0
+    change_column :drink_reviews, :drink_id, :integer, null: false, default: 0
+    change_column :drink_reviews, :body, :text, null: false, default: 'no body', limit: 1000
+    change_column :drink_reviews, :param1, :integer, null: false, default: 0
+    change_column :drink_reviews, :param2, :integer, null: false, default: 0
+  end
+  def down
+    change_column :drink_reviews, :user_id, :integer, null: false
+    change_column :drink_reviews, :drink_id, :integer, null: false
+    change_column :drink_reviews, :body, :text, null: false
+    change_column :drink_reviews, :param1, :integer, null: false
+    change_column :drink_reviews, :param2, :integer, null: false
+  end
+end
