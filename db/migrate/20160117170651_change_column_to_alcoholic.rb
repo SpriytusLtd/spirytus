cdea432d6aab2d9a69be2e698dc324540984b508
tokikaze0604 <tokikaze0604@gmail.com>
2016-01-18 02:18:29 +0900

    Alcoholicに一部間違いがあったので修正

--- db/migrate/20160117170651_change_column_to_alcoholic.rb
+++ db/migrate/20160117170651_change_column_to_alcoholic.rb
@@ -3,18 +3,18 @@ class ChangeColumnToAlcoholic < ActiveRecord::Migration
     change_column :alcoholics, :name, :text, null: false, limit: 100
     change_column :alcoholics, :item1, :text, null: false, limit: 100
     change_column :alcoholics, :item2, :text, null: false, limit: 100
-    change_column :alcoholics, :item3, limit: 100
-    change_column :alcoholics, :item4, limit: 100
-    change_column :alcoholics, :item5, limit: 100
-    change_column :alcoholics, :item6, limit: 100
+    change_column :alcoholics, :item3, :text, limit: 100
+    change_column :alcoholics, :item4, :text, limit: 100
+    change_column :alcoholics, :item5, :text, limit: 100
+    change_column :alcoholics, :item6, :text, limit: 100
   end
   def down
     change_column :alcoholics, :name, :text, null: false
     change_column :alcoholics, :item1, :text, null: false
     change_column :alcoholics, :item2, :text, null: false
-    change_column :alcoholics, :item3
-    change_column :alcoholics, :item4
-    change_column :alcoholics, :item5
-    change_column :alcoholics, :item6
+    change_column :alcoholics, :item3, :text
+    change_column :alcoholics, :item4, :text
+    change_column :alcoholics, :item5, :text
+    change_column :alcoholics, :item6, :text
   end
 end
