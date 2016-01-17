47fe5bf1b32ece77171e727d306584f86aa8aee8
gyane <gyanexus7@gmail.com>
2016-01-18 03:33:25 +0900

    indexのテストを追加

--- spec/controllers/users/reviews_controller_spec.rb
+++ spec/controllers/users/reviews_controller_spec.rb
@@ -11,6 +11,19 @@ RSpec.describe Users::ReviewsController, type: :controller do
       sign_in :user, user1
       get :index, user_id: user1.id
       expect(assigns(:store_reviews)).to match_array([review1, review2])
+      expect(response).to render_template('index')
+    end
+    it 'redirect to / if user account is different' do
+      user1 = FactoryGirl.create(:user1)
+      user2 = FactoryGirl.create(:user2)
+      sign_in :user, user1
+      get :index, user_id: user2
+      expect(response).to redirect_to('/')
+    end
+    it 'redirect to /users/sign_in if user have not logged in yet' do
+      user1 = FactoryGirl.create(:user1)
+      get :index, user_id: user1.id
+      expect(response).to redirect_to('/users/sign_in')
     end
   end
   describe 'POST #create'
