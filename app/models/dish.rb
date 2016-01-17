f7bb65c90aadade4b72fa8b1c7c16c6434eaae57
tokikaze0604 <tokikaze0604@gmail.com>
2016-01-18 04:40:53 +0900

    Dishモデルを修正

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,7 +2,7 @@ class Dish < ActiveRecord::Base
   has_and_belongs_to_many :stores
   has_and_belongs_to_many :ingredients
 
-  validatable :name, presence: true, length: { maximum: 100 }
-  validatable :local_dishes, presence: true
-  validatable :dishes, length: { maximum: 1000 }
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :local_dishes, presence: true
+  validates :dishes, length: { maximum: 1000 }
 end
