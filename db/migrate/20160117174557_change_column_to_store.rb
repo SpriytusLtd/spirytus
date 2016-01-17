4d3296af3528de732049c254fc45f2ec7210cd1e
tokikaze0604 <tokikaze0604@gmail.com>
2016-01-18 03:00:33 +0900

    Storeのmigrateで怒られた部分を修正

--- db/migrate/20160117174557_change_column_to_store.rb
+++ db/migrate/20160117174557_change_column_to_store.rb
@@ -3,7 +3,7 @@ class ChangeColumnToStore < ActiveRecord::Migration
     change_column :stores, :name, :text, null: false, limit: 100
     change_column :stores, :address, :text, null: false, limit: 200
     change_column :stores, :phone_number, :string, null: false, default: "", limit: 20
-    change_column :stores, :budget, :integer, limit: 10
+    change_column :stores, :budget, :integer
     change_column :stores, :detail, :text, null: false, limit: 2000
   end
   def down
