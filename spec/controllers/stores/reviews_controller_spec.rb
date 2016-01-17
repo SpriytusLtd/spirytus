350480742b4a98e21c93160ccff6a4032a2d4423
gyane <gyanexus7@gmail.com>
2016-01-18 01:35:10 +0900

    テストでfactory-girlに変えられるところを変えた

--- spec/controllers/stores/reviews_controller_spec.rb
+++ spec/controllers/stores/reviews_controller_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe Stores::ReviewsController, type: :controller do
   describe 'GET #index' do
     it 'redirect to the /users/sign_in' do
-      store1 = Store.create! email: 'store1@email.com', password: 'test1pass', name: 'test1', address: 'kut-1-1-1', phone_number: '090-1111-1111', budget: 900, detail: 'yeah'
+      store1 = FactoryGirl.create(:store1)
       get :index, store_id: store1.id
       expect(response).to redirect_to('/users/sign_in')
     end
     it 'contain store data in @store' do
-      store1 = Store.create! email: 'store1@email.com', password: 'test1pass', name: 'test1', address: 'kut-1-1-1', phone_number: '090-1111-1111', budget: 900, detail: 'yeah'
-      user1 = User.create! email: 'user1@email.com', password: 'user1pass'
+      store1 = FactoryGirl.create(:store1)
+      user1 = FactoryGirl.create(:user1)
       StoreReview.create! user_id: user1.id, store_id: store1.id
       StoreReview.create! user_id: user1.id, store_id: store1.id
       sign_in :user, user1
@@ -21,8 +21,8 @@ RSpec.describe Stores::ReviewsController, type: :controller do
   end
   describe 'GET #new' do
     it 'render template new' do
-      store1 = Store.create! email: 'store1@email.com', password: 'test1pass', name: 'test1', address: 'kut-1-1-1', phone_number: '090-1111-1111', budget: 900, detail: 'yeah'
-      user1 = User.create! email: 'user1@email.com', password: 'user1pass'
+      store1 = FactoryGirl.create(:store1)
+      user1 = FactoryGirl.create(:user1)
       sign_in :user, user1
       get :new, store_id: store1.id
       expect(response).to have_http_status(200)
@@ -30,15 +30,15 @@ RSpec.describe Stores::ReviewsController, type: :controller do
       expect(assigns(:review)).to be_a_new(StoreReview)
     end
     it 'redirect to the /users/sign_in if user has not sign in yet' do
-      store1 = Store.create! email: 'store1@email.com', password: 'test1pass', name: 'test1', address: 'kut-1-1-1', phone_number: '090-1111-1111', budget: 900, detail: 'yeah'
+      store1 = FactoryGirl.create(:store1)
       get :new, store_id: store1.id
       expect(response).to redirect_to('/users/sign_in')
     end
   end
   describe 'POST #create' do
     it 'create review' do
-      store1 = Store.create! email: 'store1@email.com', password: 'test1pass', name: 'test1', address: 'kut-1-1-1', phone_number: '090-1111-1111', budget: 900, detail: 'yeah'
-      user1 = User.create! email: 'user1@email.com', password: 'user1pass'
+      store1 = FactoryGirl.create(:store1)
+      user1 = FactoryGirl.create(:user1)
       sign_in :user, user1
       post :create, store_id: store1.id, store_review: { body: 'abc' }
       saved_review = StoreReview.find_by(user_id: user1.id)
