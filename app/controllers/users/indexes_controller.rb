e57e4b4ae3a3cefcd997f5756b9c242c1503bbd3
gyane <gyanexus7@gmail.com>
2016-01-18 02:33:29 +0900

    テストが通るように、users/indexesの実装を拡張

--- app/controllers/users/indexes_controller.rb
+++ app/controllers/users/indexes_controller.rb
@@ -1,5 +1,11 @@
 class Users::IndexesController < ApplicationController
+  before_action :authenticate_user!
+
   def show
-    @user = User.find(params[:id])
+    if current_user.id != params[:id].to_i
+      redirect_to '/'
+    else
+      @user = User.find(params[:id])
+    end
   end
 end
