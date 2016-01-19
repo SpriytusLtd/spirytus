d9b6fa8d060e482dba2bb6fe430efa256ee7e772
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2016-01-19 08:00:32 +0000

    favoriteを追加しました

--- app/controllers/users/favorites_controller.rb
+++ app/controllers/users/favorites_controller.rb
@@ -1,5 +1,8 @@
 class Users::FavoritesController < ApplicationController
   def index
     @user = User.find(params[:id])
+      User.favorite(user)    
+    @store = Store.find(params[:id])
+      Store.favorite(store)
   end
 end
