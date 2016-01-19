8c04bfa460fcde2b3c7b20fdedb84f56bc8aafd1
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2016-01-19 06:37:57 +0000

    configurationssを追加しました

--- app/controllers/users/configurations_controller.rb
+++ app/controllers/users/configurations_controller.rb
@@ -1,2 +1,24 @@
 class Users::ConfigurationsController < ApplicationController
+  def index
+    @users = Userfind(params[:search])   
+  end
+  
+  def create
+    @users = User.new(users_params)
+    if @users.save
+      redirect_to users_path
+    else
+      render 'create'
+    end
+  end
+
+  def new
+    @users = User.new
+  end
+
+  def destroy
+    @users = User.find(params[:id])
+    @users.destroy
+    redirect_to users_path
+  end
 end
