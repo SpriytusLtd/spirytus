bff28b83091dd928e0086fc4922438a376db432e
tokikaze0604 <tokikaze0604@gmail.com>
2016-01-22 03:23:34 +0900

    drink_reviewsアソシエーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :store_reviews, class_name: StoreReview, foreign_key: :user_id, dependent: :destroy
+  has_many :drink_reviews, class_name: DrinkReview, foreign_key: :user_id, dependent: :destroy
 
   has_and_belongs_to_many :favorite_stores, class_name: 'Store'
+  has_and_belongs_to_many :favorite_drinks, class_name: 'Drink'
 
   validates :name, presence: true, length: { maximum: 20 }
 end
