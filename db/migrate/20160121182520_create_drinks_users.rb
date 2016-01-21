757bc353ac44a58b6774efab9cf613f1e4f7dee1
tokikaze0604 <tokikaze0604@gmail.com>
2016-01-22 03:55:08 +0900

    drinksusersマイグレーションを修正

--- db/migrate/20160121182520_create_drinks_users.rb
+++ db/migrate/20160121182520_create_drinks_users.rb
@@ -1,8 +1,8 @@
 class CreateDrinksUsers < ActiveRecord::Migration
   def change
     create_table :drinks_users , id: false do |t|
-      t.references :drinks, index: true, null: false
-      t.references :users, index: true, null: false
+      t.references :drink, index: true, null: false
+      t.references :user, index: true, null: false
     end
   end
 end
