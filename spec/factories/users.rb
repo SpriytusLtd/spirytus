ffebacabdb4bf2d4b9bea830efbda892aa670391
tokikaze0604 <tokikaze0604@gmail.com>
2016-01-22 20:48:13 +0900

    factorygirlのテンプレートにname追記

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,11 @@
 FactoryGirl.define do
   factory :user1, class: User do
+    name 'user1'
     email 'user1@email.com'
     password 'user1pass'
   end
   factory :user2, class: User do
+    name 'user2'
     email 'user2@email.com'
     password 'user2pass'
   end
