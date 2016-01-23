f984dad449954f6c9955f28dac7c566180c51121
gyane <gyanexus7@gmail.com>
2016-01-23 16:35:39 +0900

    店のお気に入りの削除機能
    その前にbefore_action :authnticate_user!されていなかったのでそれを追加
    削除機能は、users/:user_id/favorites/:id
    :deleteにアクセスすることでできるようにしようと思ったが、処理速度が遅そうなので、stores/:store_id/favorites
    :deleteへのリンクを貼るという形で実装した

--- app/controllers/users/favorites_controller.rb
+++ app/controllers/users/favorites_controller.rb
@@ -1,4 +1,6 @@
 class Users::FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @user = User.find(params[:user_id])
     @favorite_stores = @user.favorite_stores
