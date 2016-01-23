3722be4b787b52a8d90efb62008c2cf15488f3bb
tokikaze0604 <tokikaze0604@gmail.com>
2016-01-24 03:56:21 +0900

    StoreとResortの関係を定義

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,5 +1,5 @@
 class Resort < ActiveRecord::Base
-  has_and_belongs_to_many :store
+  has_and_belongs_to_many :near_stores, class_name: 'Store'
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :address, length: { maximum: 200 }
