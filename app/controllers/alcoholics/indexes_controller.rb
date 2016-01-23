5851a604595b85b877b30046e0916ab0c31f76c8
gyane <gyanexus7@gmail.com>
2016-01-23 19:13:11 +0900

    alcoholicの登録・削除の実装

--- app/controllers/alcoholics/indexes_controller.rb
+++ app/controllers/alcoholics/indexes_controller.rb
@@ -1,2 +1,29 @@
 class Alcoholics::IndexesController < ApplicationController
+  before_action :authenticate_store!
+
+  def index
+    @alcoholics = Alcoholic.all
+  end
+
+  def new
+    @alcoholic = Alcoholic.new
+  end
+
+  def create
+    alcoholic = Alcoholic.new(create_params)
+    alcoholic.save
+    redirect_to action: :index
+  end
+
+  def destroy
+    alcoholic = Alcoholic.find(params[:id])
+    alcoholic.destroy
+    redirect_to action: :index
+  end
+
+  private
+
+  def create_params
+    params.require(:alcoholic).permit(:name, :item1, :item2, :item3, :item4, :item5, :item6)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,8 @@ Rails.application.routes.draw do
     resources :configurations, controller: 'users/configurations', only: [ :index, :create, :new, :destroy ]
   end
 
+  resources :alcoholics, controller: 'alcoholics/indexes', only: [ :index, :create, :new, :destroy ]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
