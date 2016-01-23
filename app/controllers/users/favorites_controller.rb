219bceb1e4b79b2b06388224612ac0f2f669ab9e
gyane <gyanexus7@gmail.com>
2016-01-24 08:51:24 +0900

    いらないファイルを削除

--- app/controllers/users/favorites_controller.rb
+++
@@ -1,8 +0,0 @@
-class Users::FavoritesController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-    @user = User.find(params[:user_id])
-    @favorite_stores = @user.favorite_stores.reviews.paginate(page: params[:page], per_page: 5)
-  end
-end
