30556db507c880945309963ad3160027d3d9114d
gyane <gyanexus7@gmail.com>
2016-01-23 21:46:28 +0900

    自動生成のテストを修正
    テストをちゃんとかいたわけではない

--- spec/controllers/brewers/indexes_controller_spec.rb
+++ spec/controllers/brewers/indexes_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Brewers::IndexesController, type: :controller do
   describe 'GET #index' do
     it 'returns http success' do
+      store1 = FactoryGirl.create(:store1)
+      sign_in :store, store1
       get :index
       expect(response).to have_http_status(:success)
     end
@@ -10,15 +12,19 @@ RSpec.describe Brewers::IndexesController, type: :controller do
 
   describe 'GET #new' do
     it 'returns http success' do
+      store1 = FactoryGirl.create(:store1)
+      sign_in :store, store1
       get :new
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET #create' do
+  describe 'POST #create' do
     it 'returns http success' do
-      get :create
-      expect(response).to have_http_status(:success)
+      store1 = FactoryGirl.create(:store1)
+      sign_in :store, store1
+      post :create, brewer: { name: '酒造' }
+      expect(response).to have_http_status(302)
     end
   end
 end
