3a5dc1723e5988dd2cb01d7d69e2540624d34b0e
tokikaze0604 <tokikaze0604@gmail.com>
2016-01-23 22:22:18 +0900

    Municipalityモデルのテストを追記

--- spec/models/municipality_spec.rb
+++ spec/models/municipality_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Municipality, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'with validation' do
+    let(:municipality) { Municipality.new(params) }
+    let(:params) do
+      hash = {}
+      hash[:name] = 'インスマス'
+      hash
+    end
+    shared_examples 'be valid' do
+      it { expect(municipality).to be_valid }
+    end
+    shared_examples 'not be valid' do
+      it { expect(municipality).not_to be_valid }
+    end
+    describe 'with true case' do
+      context 'when all present' do
+        it_behaves_like 'be valid'
+      end
+    end
+    describe 'with present' do
+      context 'when name is not present' do
+        before do
+          municipality.name = nil
+        end
+        it_behaves_like 'not be valid'
+      end
+    end
+    describe 'with maximum length' do
+      context 'when name is over limit' do
+        before do
+          str101 = 'n' * 101
+          municipality.name = str101
+        end
+        it_behaves_like 'not be valid'
+      end
+    end
+  end
 end
