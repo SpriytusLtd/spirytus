bc7d41601a2a585f9e75ecdc90e42175541db2a6
gyane <gyanexus7@gmail.com>
2016-01-27 02:09:31 +0900

    酒の種類とアルコール度数を選択できるようにした

--- app/controllers/indexes_controller.rb
+++ app/controllers/indexes_controller.rb
@@ -1,4 +1,15 @@
 class IndexesController < ApplicationController
   def index
+    @alcoholics = Alcoholic.all
+    @brewers = Brewer.all
+    @alcohol = [
+      { text: '3%以下', number: 3 },
+      { text: '5%以下', number: 5 },
+      { text: '10%以下', number: 10 },
+      { text: '15%以下', number: 15 },
+      { text: '20%以下', number: 20 },
+      { text: '40%以下', number: 40 },
+      { text: '41%以上', number: 100 }
+    ]
   end
 end
