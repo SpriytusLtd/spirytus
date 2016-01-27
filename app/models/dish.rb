333f0b356ab660c425897d3977d3ab65da1e6a19
tokikaze0604 <tokikaze0604@gmail.com>
2016-01-27 10:10:31 +0900

    コンフリクト修正

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,5 +1,6 @@
 class Dish < ActiveRecord::Base
-  has_and_belongs_to_many :stores, class_name: 'Store'
+  has_many :store_dishes, class_name: 'StoreDish', foreign_key: :dish_id, dependent: :destroy
+  has_many :belong_stores, class_name: 'Store', through: :store_dishes
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :detail, length: { maximum: 1000 }
--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,14 +1,13 @@
 class Drink < ActiveRecord::Base
   has_many :reviews, class_name:  'DrinkReview', foreign_key: :drink_id, dependent: :destroy
   has_many :users_who_reviews, class_name: 'User', through: :reviews
-  has_many :store_drinks
-  has_many :stores, through: :store_drinks
+  has_many :store_drinks, class_name: 'StoreDrink', foreign_key: :drink_id, dependent: :destroy
+  has_many :belong_stores, class_name: 'Store', through: :store_drinks
 
   belongs_to :brewer, foreign_key: :brewer_id
   belongs_to :alcoholic, foreign_key: :alcoholic_id
 
   has_and_belongs_to_many :users_who_likes, class_name: 'User'
-  has_and_belongs_to_many :stores, class_name: 'Store'
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :alcohol, length: { maximum: 3 }
