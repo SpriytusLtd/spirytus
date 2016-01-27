42bf643cf2337b34c3f0b168ecfe6876c03312f4
gyane <gyanexus7@gmail.com>
2016-01-27 11:15:47 +0900

    アルコール度数の段階の番号のつけ方を変更

--- app/controllers/indexes_controller.rb
+++ app/controllers/indexes_controller.rb
@@ -3,11 +3,11 @@ class IndexesController < ApplicationController
     @alcoholics = Alcoholic.all
     @brewers = Brewer.all
     @alcohol = [
-      { text: '4%以下', number: 1 },
-      { text: '9%以下', number: 2 },
-      { text: '19%以下', number: 3 },
-      { text: '39%以下', number: 4 },
-      { text: '40%以上', number: 5 }
+      { text: '4%以下', number: 0 },
+      { text: '9%以下', number: 1 },
+      { text: '19%以下', number: 2 },
+      { text: '39%以下', number: 3 },
+      { text: '40%以上', number: 4 }
     ]
     @dishes = Dish.all
     @resorts = Resort.all
