d1c04c6b554c6a1c794bf66b3dd5c04a96713316
tokikaze0604 <tokikaze0604@gmail.com>
2016-02-01 12:46:50 +0900

    ロベルトのシード追加

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,6 +1,6 @@
 class Resort < ActiveRecord::Base
   has_many :resort_stores, class_name: 'ResortStore', foreign_key: :resort_id, dependent: :destroy
-  has_many :close_stores, class_name: 'Store', through: :resort_stores
+  has_many :close_stores, class_name: 'Store', through: :resort_stores, source: :store
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :address, length: { maximum: 200 }
